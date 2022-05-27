b280ee714515ea3f0c867329392ebe29eef34dae
MaximBabenkov <lostbass@mail.ru>
2022-05-27 11:35:51 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// В одномерном массиве из 123 чисел
+// найти количество элементов из отрезка [10,99]
+int[]arr = new int[123];
+for (int i=0;i<arr.Length;i++)
+{
+    arr[i]=new Random().Next(0,1000);
+    Console.Write($"{arr[i]} ");
+}
+int sum=0;
+Console.WriteLine();
+for (int i=0;i<arr.Length;i++) 
+{
+    if (arr[i]>=10 && arr[i]<=99) sum=sum+1;
+}
+Console.Write($"The amount of numbers from range [10,99]: {sum}");
