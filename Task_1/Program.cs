5fc528ee027f483eb70cf4d7bfbddfa0283218f8
MaximBabenkov <lostbass@mail.ru>
2022-05-27 11:25:33 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задать массив из 8 элементов, заполнить нолями и единицами, вывести на экран
+int[]arr = new int[8];
+for (int i=0;i<arr.Length;i++)
+{
+    arr[i]=new Random().Next(0,2);
+    Console.Write($"{arr[i]} ");
+}
